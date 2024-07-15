51ef3ee06c6ccd78b6cb21c9886c36a868edf617
Niranjan Vi <157526421+Niranjan-Vi@users.noreply.github.com>
2024-07-15 21:47:39 +0530

    initial commit

--- admin.js
+++ admin.js
@@ -3,14 +3,22 @@ let team1 = document.getElementById('team1');
 let team2 = document.getElementById('team2');
 let over = document.getElementById('over');
 let run = document.querySelectorAll('.score-button');
-          
+let player1 = document.getElementById('player1');
+let player2 = document.getElementById('player2');
+// Variables 
+
+
+let over_count = 0;
 let runs = [];
-let count=0;
+let count = 0;
+let first_player = "";
+let second_player = "";
 // Admin operations
 
 update = document.getElementById('update');
 update.addEventListener('click', () => {
     add();
+    player_details();
 });
 function add() {
     console.log(team1.value);
@@ -20,31 +28,60 @@ function add() {
 run.forEach(button => {
     button.addEventListener('click', () => {
         scorecalculation(button);
-        if(count==6){
+        if (count == 6) {
             console.log("over completed");
+            over_count++;
+            over.value = over_count + "." + count;
+            count = 0;
 
-            
         }
-        
+
     });
 
 });
 
 function scorecalculation(button) {
-    console.log(button.innerHTML);
-    if (button.innerHTML == "W") {
+    // console.log(button.innerHTML);
+    if (button.innerHTML == 'W') {
+        console.log("wicket");
         runs.push(0);
         count++;
+
     }
-    if (button.innerHTML == 'WD') {
+    else if (button.innerHTML == 'WD') {
         // s = parseInt(button.innerHTML);
         runs.push(1);
     }
-    if (button.innerHTML != 'WD') {
+    else if (button.innerHTML != 'WD') {
         s = parseInt(button.innerHTML);
         runs.push(s);
         count++;
+        let count1 = count;
+        over.value = over_count + "." + count;
     }
+    //ball count
+    if (runs[runs.length - 1] <= 3 ) {
+        if (button.innerHTML == 'W') {
+            if (player1.style.color = "red") {
+                console.log("player1 got out");
+                player1.value = " ";
+            }
+            else {
+                console.log("player2 got out");
+                player2.value = " ";
+            }
+        }
+        else if (!(count % 2 == 0)) {
+            player2.style.backgroundColor = "transparent";
+            player1.style.backgroundColor = "red";
+
+        } else {
+            player2.style.backgroundColor = "red";
+            player1.style.backgroundColor = "transparent";
+
+        }
+    }
+
     runcount();
 
 }
@@ -54,5 +91,10 @@ function runcount() {
     for (let i = 0; i < runs.length; i++) {
         sum += runs[i];
     }
+
     console.log("total run is" + sum);
+}
+function player_details() {
+    player1.value;
+    player2.value;
 }
